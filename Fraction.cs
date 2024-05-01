fc719ee6376ba3c5e65dbe9322e1e2889695c0e9
Kvazarggg <makcumkagg@gmail.com>
2024-05-01 16:12:23 +0300

    Добавьте файлы проекта.

---
+++ Fraction.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Fraction
+    {
+        private int numerator;
+        private int denominator;
+
+        public Fraction(int num, int denom)
+        {
+            numerator = num;
+            denominator = denom;
+        }
+
+        // Метод для сокращения дроби
+        public void Reduce()
+        {
+            int gcd = GCD(Math.Abs(numerator), Math.Abs(denominator));
+            numerator /= gcd;
+            denominator /= gcd;
+        }
+
+        // Метод вывода дроби
+        public void PrintFraction()
+        {
+            Console.WriteLine($"{numerator}/{denominator}");
+        }
+
+        // Операторы перегрузки для сложения, вычитания, умножения и деления дробей
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            int num = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
+            int denom = f1.denominator * f2.denominator;
+            return new Fraction(num, denom);
+        }
+
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            int num = f1.numerator * f2.denominator - f2.numerator * f1.denominator;
+            int denom = f1.denominator * f2.denominator;
+            return new Fraction(num, denom);
+        }
+
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            int num = f1.numerator * f2.numerator;
+            int denom = f1.denominator * f2.denominator;
+            return new Fraction(num, denom);
+        }
+
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            int num = f1.numerator * f2.denominator;
+            int denom = f1.denominator * f2.numerator;
+            return new Fraction(num, denom);
+        }
+
+        // Метод для вычисления наибольшего общего делителя
+        private static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Исходные данные
+            Fraction fraction1 = new Fraction(1, 2);
+            Fraction fraction2 = new Fraction(3, 4);
+            Fraction fraction3 = new Fraction(5, 6);
+            Fraction fraction4 = new Fraction(7, 8);
+            Fraction fraction5 = new Fraction(9, 10);
+
+            // Вычисление выражения
+            Fraction result = (((fraction1 + fraction2) / fraction3) * (fraction4 - fraction5));
+
+            // Вывод несокращенной дроби
+            Console.WriteLine("Несокращенная дробь:");
+            result.PrintFraction();
+
+            // Сокращение дроби и вывод
+            result.Reduce();
+            Console.WriteLine("Сокращенная дробь:");
+            result.PrintFraction();
+        }
+    }
+}
